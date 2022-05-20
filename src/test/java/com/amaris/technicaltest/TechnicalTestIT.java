c3e3c1bd69c26e85615e13645ebfdda97315deb2
Carlos1991 <carlosma_1991@hotmail.com>
2022-05-20 12:54:53 +0200

    Reuse code in integration tests

--- src/test/java/com/amaris/technicaltest/TechnicalTestIT.java
+++ src/test/java/com/amaris/technicaltest/TechnicalTestIT.java
@@ -23,104 +23,61 @@ class TechnicalTestIT {
 
     @Test
     void get_day14Hour10_productId35455_brandId1() throws IOException {
-        webTestClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .path("/prices")
-                        .queryParam("applicationDateTime", "2020-06-14T10:00:00")
-                        .queryParam("productId", 35455)
-                        .queryParam("brandId", 1)
-                        .build())
-                .accept(MediaType.APPLICATION_NDJSON)
-                .exchange()
+        executeGetPricesRequest("2020-06-14T10:00:00", 35455, 1)
                 .expectStatus().isOk()
                 .expectBody().json(FileReader.readAsString("json/Price1.json"));
     }
 
     @Test
     void get_day14Hour16_productId35455_brandId1() throws IOException {
-        webTestClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .path("/prices")
-                        .queryParam("applicationDateTime", "2020-06-14T16:00:00")
-                        .queryParam("productId", 35455)
-                        .queryParam("brandId", 1)
-                        .build())
-                .accept(MediaType.APPLICATION_NDJSON)
-                .exchange()
+        executeGetPricesRequest("2020-06-14T16:00:00", 35455, 1)
                 .expectStatus().isOk()
                 .expectBody().json(FileReader.readAsString("json/Price2.json"));
     }
 
     @Test
     void get_day14Hour21_productId35455_brandId1() throws IOException {
-        webTestClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .path("/prices")
-                        .queryParam("applicationDateTime", "2020-06-14T23:00:00")
-                        .queryParam("productId", 35455)
-                        .queryParam("brandId", 1)
-                        .build())
-                .accept(MediaType.APPLICATION_NDJSON)
-                .exchange()
+        executeGetPricesRequest("2020-06-14T23:00:00", 35455, 1)
                 .expectStatus().isOk()
                 .expectBody().json(FileReader.readAsString("json/Price1.json"));
     }
 
     @Test
     void get_day15Hour10_productId35455_brandId1() throws IOException {
-        webTestClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .path("/prices")
-                        .queryParam("applicationDateTime", "2020-06-15T10:00:00")
-                        .queryParam("productId", 35455)
-                        .queryParam("brandId", 1)
-                        .build())
-                .accept(MediaType.APPLICATION_NDJSON)
-                .exchange()
+        executeGetPricesRequest("2020-06-15T10:00:00", 35455, 1)
                 .expectStatus().isOk()
                 .expectBody().json(FileReader.readAsString("json/Price3.json"));
     }
 
     @Test
     void get_day15Hour21_productId35455_brandId1() throws IOException {
-        webTestClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .path("/prices")
-                        .queryParam("applicationDateTime", "2020-06-15T21:00:00")
-                        .queryParam("productId", 35455)
-                        .queryParam("brandId", 1)
-                        .build())
-                .accept(MediaType.APPLICATION_NDJSON)
-                .exchange()
+        executeGetPricesRequest("2020-06-15T21:00:00", 35455, 1)
                 .expectStatus().isOk()
                 .expectBody().json(FileReader.readAsString("json/Price4.json"));
     }
 
     @Test
-    void get_missingRequiredParameter() throws IOException {
-        webTestClient.get()
-                .uri(uriBuilder -> uriBuilder
-                        .path("/prices")
-                        .queryParam("applicationDateTime", "2020-06-15T21:00:00")
-                        .queryParam("productId", 35455)
-                        .build())
-                .accept(MediaType.APPLICATION_NDJSON)
-                .exchange()
+    void get_missingRequiredParameter() {
+        executeGetPricesRequest("2020-06-15T21:00:00", 35455, null)
                 .expectStatus().isBadRequest();
     }
 
     @Test
-    void get_invalidParameterFormat() throws IOException {
-        webTestClient.get()
+    void get_invalidParameterFormat() {
+        executeGetPricesRequest("01-01-2020", 35455, 1)
+                .expectStatus().isBadRequest();
+    }
+
+    private WebTestClient.ResponseSpec executeGetPricesRequest(String applicationDateTime, Integer productId, Integer brandId) {
+        return webTestClient.get()
                 .uri(uriBuilder -> uriBuilder
                         .path("/prices")
-                        .queryParam("applicationDateTime", "01-01-2020")
-                        .queryParam("productId", 35455)
-                        .queryParam("brandId", 1)
+                        .queryParam("applicationDateTime", applicationDateTime)
+                        .queryParam("productId", productId)
+                        .queryParam("brandId", brandId)
                         .build())
                 .accept(MediaType.APPLICATION_NDJSON)
-                .exchange()
-                .expectStatus().isBadRequest();
+                .exchange();
     }
 
 }
